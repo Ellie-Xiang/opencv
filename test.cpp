a2f6bd96edd73c3b9c53a905f99c832ce9001440
Ellie Xiang <todayis_xal615@163.com>
2020-06-17 18:34:30 +0800

    opencv test

---
+++ test.cpp
@@ -0,0 +1,19 @@
+﻿
+#include <iostream>  
+#include <opencv2/core/core.hpp>  
+#include <opencv2/highgui/highgui.hpp>  
+
+using namespace cv;
+
+int main()
+{
+	// 读入一张图片（poyanghu缩小图）    
+	Mat img = imread("F:\\ilil\\cool\\44366166_p0.jpg");
+	// 创建一个名为 "图片"窗口    
+	namedWindow("图片",WINDOW_AUTOSIZE);
+	// 在窗口中显示图片   
+	imshow("图片", img);   
+	waitKey(0);
+	destroyWindow("图片");
+	return 0;
+}
