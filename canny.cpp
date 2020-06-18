5b16609ea74cf1b118efcaf34e792742b2e858fe
Ellie Xiang <todayis_xal615@163.com>
2020-06-18 12:07:23 +0800

    Finding edges done

--- canny.cpp
+++ canny.cpp
@@ -4,28 +4,30 @@
 #include <iostream>
 using namespace cv;
 using namespace std;
-Mat g_grey, g_binary;
+Mat g_gray, g_binary;
 int g_thresh = 100;
 void on_trackbar(int , void*)
 {
+	Canny(g_gray, g_binary, g_thresh, 255);
 	vector< vector<cv::Point> > contours;
-	threshold(g_grey, g_binary, g_thresh, 255, THRESH_BINARY);
    	findContours(g_binary, contours, noArray(), RETR_LIST, CHAIN_APPROX_SIMPLE);
 	g_binary = Scalar::all(0);
 	drawContours(g_binary, contours, -1, cv::Scalar::all(255));
-	imshow("Contours", g_binary);
+	cv::imshow("Contours", g_binary);
 }
 int main(int argc,char** argv)
 {
-	if ((g_grey = imread(argv[1],0)).empty())
+	/*if (argc!=2 || (g_gray = imread(argv[1],0)).empty())
 	{
-		cout << "Find threshold dependent contours\nUsage:" << argv[0] << "F:\\ilil\\cool\\44366166_p0.jpg" << endl;
-	};
-	namedWindow("Contours", WINDOW_AUTOSIZE);
+		cout << "Find threshold dependent contours\nUsage:" << argv[0] << "F:\\ilil\\me\\1.jpg" << endl;
+	};*/
+	g_gray = imread("F:\\ilil\\me\\1.jpg");
+	namedWindow("Contours", 1);
 	// 在窗口中显示图片   
+	//imshow("Contours",g_gray);
 	createTrackbar("Threshold", "Contours", &g_thresh, 255, on_trackbar);
-
-	on_trackbar(0,0);
+	             
+	on_trackbar(0,0);  
 
 	waitKey();
 	return 0;
--- test.cpp
+++ test.cpp
@@ -1,19 +1,23 @@
 ﻿
 #include <iostream>  
 #include <opencv2/core/core.hpp>  
+#include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>  
 
 using namespace cv;
+using namespace std;
 
 int main()
 {
 	// 读入一张图片（poyanghu缩小图）    
 	Mat img = imread("F:\\ilil\\cool\\44366166_p0.jpg");
+	Mat gray;
 	// 创建一个名为 "图片"窗口    
 	namedWindow("图片",WINDOW_AUTOSIZE);
+	Canny(img,gray, 100, 255);
 	// 在窗口中显示图片   
-	imshow("图片", img);   
+	imshow("图片", gray);   
 	waitKey(0);
-	destroyWindow("图片");
+	destroyWindow("a");
 	return 0;
 }
